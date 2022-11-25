a910a610e431ee3225aaa8ef90413527a0fc40b8
Wirebee2751 <119036568+Wirebee2751@users.noreply.github.com>
2022-11-25 14:19:30 +0530

    greatest of 3 number

---
+++ greatestthreenumbers.py
@@ -0,0 +1,13 @@
+a = 155
+b = 255
+c = 355
+if (a >= b) and (a >= c):
+    largest = a
+    print(largest)
+if (b >= a) and (b >= c):
+    largest = b
+    print(largest)
+else:
+    largest = c
+    print(largest)
+    
